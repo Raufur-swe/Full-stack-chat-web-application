ed6cde93687019bf2c050948c72b099d1c7f5296
Raufur <raufurshekh45@gmail.com>
2025-11-30 18:51:22 +0600

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
+import path from "path"
 import authRoute from "./routes/auth.route.js"
 import massageRoute from "./routes/massage.route.js"
 
-
-
-
-
 dotenv.config();
-const PORT = process.env.PORT || 8000;
 const app = express()
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 8000;
 
 //auth route
 app.use("/api/auth" , authRoute);
@@ -19,6 +17,13 @@ app.use("/api/massage" , massageRoute);
 
 
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    
+    app.all("/*pathMatch", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
+
+
 app.listen(PORT , ()=>console.log("Server running on port:", +PORT));
-
-
