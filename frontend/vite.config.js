d3888c98910376d7a7cd719f58726818e86b6bca
Raufur <raufurshekh45@gmail.com>
2026-01-02 20:49:01 +0600

    frontendd-setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import daisyui from 'daisyui'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss(),daisyui()],
   server:{
     host:"0.0.0.0",
     port:3000
